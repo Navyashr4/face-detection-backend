d9102ecb00c8de3396a1cecda464464f70df242e
Navya Shree <navyashr4@gmail.com>
2022-06-22 12:15:36 +0530

    ssl error fixing to address unable to register

--- server.js
+++ server.js
@@ -11,9 +11,11 @@ import { handleApiCall, handleImage } from './controllers/image.js';
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl:true
-    }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    },
 });
 
 const app = express();
